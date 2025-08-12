3fe4f2331f19faea707f1e6b84a1ff42425cc314
Varija22 <varija.korti.btech2024@sitpune.edu.in>
2025-08-12 15:23:21 +0530

    Update Exp10-A.cpp

--- Exp10-A.cpp
+++ Exp10-A.cpp
@@ -1,1 +1,18 @@
+//Varija K
+//24070123165
+#include <iostream>
+using namespace std;
 
+void swap(int& x, int& y) {
+    int temp = x;
+    x = y;
+    y = temp;
+}
+
+int main() {
+    int a = 5, b = 2;
+    swap(a, b);
+    cout << "Value of a is: " << a << endl;
+    cout << "Value of b is: " << b << endl;
+    return 0;
+}
