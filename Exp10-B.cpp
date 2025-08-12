e79e161c5dce1c17def010036671abe0cab05a01
Varija22 <varija.korti.btech2024@sitpune.edu.in>
2025-08-12 15:24:27 +0530

    Update Exp10-B.cpp

--- Exp10-B.cpp
+++ Exp10-B.cpp
@@ -1,1 +1,19 @@
+// Varija K
+// 24070123165
+#include <iostream>
+using namespace std;
+
+void increment(int &S)
+{
+    S=S+5000;
+    cout<<"Salary incremented: "<<S<<endl;
+}
+
+int main() {
+    int sal=27000;
+  
+    cout << "Value of salary is: " << sal << endl;
+      increment(sal);
+    return 0;
+}
 
